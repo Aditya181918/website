5fc241351613e3f05dc6f905054badbe5586faa0
Aditya Solanki <adityasolanki@MacBook-Air-6.local>
2026-05-12 09:01:46 +0530

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { Analytics } from '@vercel/analytics/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+    <Analytics />
+  </React.StrictMode>,
 )
